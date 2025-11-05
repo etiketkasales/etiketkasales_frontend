ccfe7122600fc0eabac35843c476dd12d3228a4d
avocadina <zhavoronka2019@gmail.com>
2025-11-05 16:33:59 +0300

    update profile avatar

--- src/entities/profile-section/ui/content/personal/change/button/index.tsx
+++ src/entities/profile-section/ui/content/personal/change/button/index.tsx
@@ -10,15 +10,15 @@ interface Props {
 
 export default function ProfileChangeButton({ onSave, disabled }: Props) {
   return (
-    <Button
-      typeButton="yellow"
-      size="16-24"
-      onClick={onSave}
-      disabled={disabled}
-      className={classes.container}
-      radius={12}
-    >
-      <span className="text-yellow-1000 heading h7">Сохранить</span>
-    </Button>
+      <Button
+        typeButton="yellow"
+        size="16-24"
+        onClick={onSave}
+        disabled={disabled}
+        className={classes.container}
+        radius={12}
+      >
+        <span className="text-yellow-1000 heading h7">Сохранить</span>
+      </Button>
   );
 }
