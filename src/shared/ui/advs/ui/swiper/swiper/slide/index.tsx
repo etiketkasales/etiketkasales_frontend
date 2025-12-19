37ca9512d55326d990342b76588b2e2ff28864f3
avocadina <zhavoronka2019@gmail.com>
2025-12-19 14:18:47 +0300

    fix header user profile image design

--- src/shared/ui/advs/ui/swiper/swiper/slide/index.tsx
+++ src/shared/ui/advs/ui/swiper/swiper/slide/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useWindowSize } from "react-use";
 
+import classes from "./slide.module.scss";
 import LinkContainer from "~/src/shared/ui/link-container/ui";
 import ImageWrapper from "~/src/shared/ui/image-wrapper/ui";
 import { IAdv } from "~/src/shared/ui/advs/model/advs.interface";
@@ -16,8 +17,9 @@ export default function AdvsSlide({ image_url, link }: Props) {
       <ImageWrapper
         src={image_url ? image_url : "/adv/no-add-banner.png"}
         width={1240}
-        height={width <= 360 ? 200 : 464}
+        height={width <= 420 ? 200 : 464}
         alt="Рекламный баннер"
+        className={classes.image}
         priority
       />
     </LinkContainer>
